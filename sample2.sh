a85c40a157317c10105bd8d03b9dd310d0bace0f
jaysonjhill <jayson.j.b.bondad@accenture.com>
2016-11-25 12:35:20 +0800

    1

---
+++ sample2.sh
@@ -0,0 +1,12 @@
+sudo su
+yum update -y
+yum install git -y
+sudo touch /etc/yum.repos.d/docker.repo
+printf "[dockerrepo] \nname=Docker Repository \nbaseurl=https://yum.dockerproject.org/repo/main/centos/7/ \nenabled=1 \ngpgcheck=1 \ngpgkey=https://yum.dockerproject.org/gpg" > /etc/yum.repos.d/docker.repo
+yum install docker-engine -y 
+systemctl docker start
+chkconfig docker on
+curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose && ln -sv /usr/local/bin/docker-compose /usr/bin/docker-compose
+mkdir /adop && cd /adop
+git clone https://github.com/AccenturePDC/adop-docker-compose.git -b pdc-ext
