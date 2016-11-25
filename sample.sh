d955aac5f2d735ff1c486769e863939c969a9c95
jaysonjhill <jayson.j.b.bondad@accenture.com>
2016-11-25 13:48:34 +0800

    1

--- sample.sh
+++ sample.sh
@@ -4,7 +4,7 @@ yum install git -y
 sudo touch /etc/yum.repos.d/docker.repo
 printf "[dockerrepo] \nname=Docker Repository \nbaseurl=https://yum.dockerproject.org/repo/main/centos/7/ \nenabled=1 \ngpgcheck=1 \ngpgkey=https://yum.dockerproject.org/gpg" > /etc/yum.repos.d/docker.repo
 yum install docker-engine -y 
-systemctl docker start
+service docker start
 chkconfig docker on
 curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose && ln -sv /usr/local/bin/docker-compose /usr/bin/docker-compose
