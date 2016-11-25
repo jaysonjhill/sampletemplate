40a56253296591be59153fa45a30bab166642c80
jaysonjhill <jayson.j.b.bondad@accenture.com>
2016-11-25 17:40:11 +0800

    1

--- sample2.sh
+++ sample2.sh
@@ -1,7 +1,7 @@
-yum update -y
+#sudo yum update -y
 #sudo yum install git -y
-#sudo touch /etc/yum.repos.d/docker.repo
-#sudo printf "[dockerrepo] \nname=Docker Repository \nbaseurl=https://yum.dockerproject.org/repo/main/centos/7/ \nenabled=1 \ngpgcheck=1 \ngpgkey=https://yum.dockerproject.org/gpg" > /etc/yum.repos.d/docker.repo
+sudo touch /etc/yum.repos.d/docker.repo
+sudo printf "[dockerrepo] \nname=Docker Repository \nbaseurl=https://yum.dockerproject.org/repo/main/centos/7/ \nenabled=1 \ngpgcheck=1 \ngpgkey=https://yum.dockerproject.org/gpg" > /etc/yum.repos.d/docker.repo
 #sudo yum install docker-engine -y 
 #sudo service docker start
 #sudo chkconfig docker on
