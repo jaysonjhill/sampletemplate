3c506be0c509265271947c5c98cce98e3aec166c
jaysonjhill <jayson.j.b.bondad@accenture.com>
2016-11-25 17:55:52 +0800

    1

--- sample2.sh
+++ sample2.sh
@@ -1,11 +1,1 @@
-#sudo yum update -y
-yum install git -y
-#touch /etc/yum.repos.d/docker.repo
-#printf "[dockerrepo] \nname=Docker Repository \nbaseurl=https://yum.dockerproject.org/repo/main/centos/7/ \nenabled=1 \ngpgcheck=1 \ngpgkey=https://yum.dockerproject.org/gpg" > /etc/yum.repos.d/docker.repo
-#sudo yum install docker-engine -y 
-#sudo service docker start
-#sudo chkconfig docker on
-#sudo curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-#sudo chmod +x /usr/local/bin/docker-compose && ln -sv /usr/local/bin/docker-compose /usr/bin/docker-compose
-#sudo mkdir /adop && cd /adop
-#sudo git clone https://github.com/rhubel123/adop-docker-compose-hcm.git
+sudo su
