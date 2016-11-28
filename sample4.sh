8d5e12d97c2f770193c83c686bfa721eb81b1ca3
jaysonjhill <jayson.j.b.bondad@accenture.com>
2016-11-28 20:18:04 +0800

    1

--- sample4.sh
+++ sample4.sh
@@ -4,6 +4,22 @@ git clone https://github.com/jaysonjhill/adop-docker-compose.git -b master
 chmod -R 700 /adop/adop-docker-compose
 cd /adop/adop-docker-compose
 mkdir -p conf/provider/oracle
+# Example file to store all the generated secrets for your ADOP stack
+
+# Username for initial admin user
+export INITIAL_ADMIN_USER="john.smith"
+
+# Password for initial admin user
+export INITIAL_ADMIN_PASSWORD_PLAIN="Password01"
+
+# Password for the Jenkins user
+export PASSWORD_JENKINS="1e6a8dd7fe461209"
+
+# Password for the Gerrit user
+export PASSWORD_GERRIT="127893c312264915"
+
+# Root password for the Sonar and Gerrit MySQL instances
+export PASSWORD_SQL="65c25aa649ac5214"
 source ./conf/env.provider.sh
 source ./credentials.generate.sh
 source ./env.config.sh
