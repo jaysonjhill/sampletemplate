33280f67dda49b9f24609b8dcfb78053407a9995
jaysonjhill <jayson.j.b.bondad@accenture.com>
2016-11-28 17:23:35 +0800

    1

--- sample.sh
+++ sample.sh
@@ -8,9 +8,9 @@ chkconfig docker on
 curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose && ln -sv /usr/local/bin/docker-compose /usr/bin/docker-compose
 mkdir /adop && cd /adop
-git clone https://github.com/rhubel123/adop-docker-compose-hcm.git
-chmod -R 700 /adop/adop-docker-compose-hcm
-cd /adop/adop-docker-compose-hcm
+git clone https://github.com/pdcbuckets/adop-docker-compose.git -b master
+chmod -R 700 /adop/adop-docker-compose
+cd /adop/adop-docker-compose
 mkdir -p conf/provider/oracle
 export PUBLIC_IP=$(curl ifconfig.co)
 export IP=$(hostname --ip-address) 
@@ -20,13 +20,10 @@ export CUSTOM_NETWORK_NAME=docker-network
 docker network create ${CUSTOM_NETWORK_NAME}
 docker-compose -f compose/elk.yml up -d
 source ./conf/env.provider.sh
-pwd
 source ./credentials.generate.sh
 source ./env.config.sh
 docker-compose pull
 docker-compose -f docker-compose.yml -f etc/volumes/local/default.yml up -d
-docker exec -it proxy bash
-service php5-fpm start
-exit
-docker exec -it proxy service nginx restart
+#docker exec -it proxy service php5-fpm start
+#docker exec -it proxy service nginx restart
 echo "====DONE===="
