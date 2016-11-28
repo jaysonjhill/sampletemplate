d3b3a26f6f929d8ec0812eb935a3221f472e479e
jaysonjhill <jayson.j.b.bondad@accenture.com>
2016-11-28 13:34:33 +0800

    1

--- sample.sh
+++ sample.sh
@@ -1,7 +1,6 @@
-sudo su
-yum update -y
+yum update -y --exclude=WALinuxAgent
 yum install git -y
-sudo touch /etc/yum.repos.d/docker.repo
+touch /etc/yum.repos.d/docker.repo
 printf "[dockerrepo] \nname=Docker Repository \nbaseurl=https://yum.dockerproject.org/repo/main/centos/7/ \nenabled=1 \ngpgcheck=1 \ngpgkey=https://yum.dockerproject.org/gpg" > /etc/yum.repos.d/docker.repo
 yum install docker-engine -y 
 service docker start
@@ -20,16 +19,13 @@ export LOGSTASH_HOST=${IP}
 export CUSTOM_NETWORK_NAME=docker-network
 docker network create ${CUSTOM_NETWORK_NAME}
 docker-compose -f compose/elk.yml up -d
-sleep 5m
 source ./conf/env.provider.sh
 source credentials.generate.sh
 source env.config.sh
 docker-compose pull
-sleep 20m
 docker-compose -f docker-compose.yml -f etc/volumes/local/default.yml up -d
-sleep 5m
 docker exec -it proxy bash
 service php5-fpm start
-exit\n
+exit
 docker exec -it proxy service nginx restart
-mkdir done
+echo "====DONE===="
